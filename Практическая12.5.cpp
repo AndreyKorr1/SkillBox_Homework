d26c6d513f3792b1ba81a739770783c2144c5f80
AndreyKor <AndreyKorr1@yandex.ru>
2024-10-24 15:43:43 +0300

    практика 12.5

---
+++ Практическая12.5.cpp
@@ -0,0 +1,126 @@
+﻿#include <string>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+
+void heapify(float arr[], int n, int i)
+{
+	int min = i;
+	int l = 2 * i + 1; 
+	int r = 2 * i + 2; 
+
+	if (l < n && arr[l] < arr[min])
+		min = l;
+
+	if (r < n && arr[r] < arr[min]) //поменял условия для сортировки массива от большего числа к меньшему
+		min = r;
+
+	if (min != i)
+	{
+		swap(arr[i], arr[min]);
+
+		heapify(arr, n, min);
+	}
+}
+
+void heapSort(float arr[], int n)
+{
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(arr, n, i);
+
+	for (int i = n - 1; i >= 0; i--)
+	{
+		swap(arr[0], arr[i]);
+
+		heapify(arr, i, 0);
+	}
+}
+
+void printArray(float arr[], int n)
+{
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+float travelTime(float distance, float speed) {
+	assert(speed > 0);
+	return distance / speed;
+}
+
+int arrSum(int arr[]) {
+	int sum = 0;
+	for (int i = 0; i < 15; i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+
+int minNumber(int arr[]) {
+	int min = arr[0];
+	for (int i = 0; i < 15; i++) {
+		if (min > arr[i]) {
+			min = arr[i];
+		}
+	}
+	return min;
+}
+
+
+
+int main()
+{
+	//Задание 1
+	/*string residents[10];
+
+	for (int i = 0; i < 10; i++) {
+		string surname;
+		cout << "Enter resident number " << i + 1 << " :";
+		cin >> surname;
+		residents[i] = surname;
+	}
+
+	for (int i = 0; i < 3; i++) {
+		int flat;
+		cout << "Enter flat number :";
+		cin >> flat;
+		if (flat > 10 || flat < 1) {
+			cout << "Wrong flat number";
+		}
+		else {
+			cout << residents[flat - 1] << endl;
+		}
+	}*/
+	//Задание 2
+
+	/*float numbers[15];
+
+	cout << "Input : ";
+
+	for (int i = 0; i < 15; i++) {
+		cin >> numbers[i];
+	}
+
+	int n = sizeof(numbers) / sizeof(numbers[0]);
+
+	heapSort(numbers, n);
+
+	cout << "Sorted array is \n";
+	printArray(numbers, n);*/
+
+	//Задание 3
+	/*float distance, speed;
+	cout << "Input distance and speed :";
+	cin >> distance >> speed;
+
+	cout << "Travel  time is " << travelTime(distance, speed) << endl;*/
+
+	//задание 4
+
+	int numbers[15] = { 114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118 };
+	int trueSum = (minNumber(numbers) + minNumber(numbers) + 13) * 14 / 2;
+	cout << "Recurring number is " << arrSum(numbers) - trueSum;
+
+}
